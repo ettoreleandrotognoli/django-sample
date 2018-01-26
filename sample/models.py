91993e3731d35328fcbf8898f034063ee47397f1
Éttore Leandro Tognoli <ettore.tognoli@ciag.org.br>
2018-01-26 12:07:19 -0200

    i18n

--- sample/models.py
+++ sample/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Movement(models.Model):
+    class Meta:
+        verbose_name = _('Movimento')
+        verbose_name_plural = _('Movimentos')
+
     created = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+        verbose_name=_('Criado em'),
     )
     value = models.DecimalField(
         max_digits=10,
-        decimal_places=2
+        decimal_places=2,
+        verbose_name=_('Valor')
     )
-    remark = models.TextField()
+    remark = models.TextField(
+        verbose_name=_('Observação')
+    )
